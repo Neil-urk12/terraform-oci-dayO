7fa353ef33b43acea87283003c392fac94a6fcc8
Neil Vallecer <140867360+Neil-urk12@users.noreply.github.com>
2025-12-08 16:10:58 +0800

    feat: add compute instance with public IP and Ubuntu 24.04 in main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,72 @@
-  terraform {
-    required_providers {
-      oci = {
-        source = "oracle/oci"
-      }
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
     }
   }
+}
 
-  provider "oci" {
-    region              = var.region
-    auth                = "SecurityToken"
-    config_file_profile = "learn-terraform"
+provider "oci" {
+  region              = var.region
+  auth                = "SecurityToken"
+  config_file_profile = "learn-terraform"
+}
+
+resource "oci_core_vcn" "internal" {
+  dns_label      = "internal"
+  cidr_block     = "172.16.0.0/20"
+  compartment_id = var.compartment_id
+  display_name   = "My internal VCN"
+}
+
+resource "oci_core_subnet" "dev" {
+  vcn_id                     = oci_core_vcn.internal.id
+  cidr_block                 = "172.16.0.0/24"
+  compartment_id             = var.compartment_id
+  display_name               = "Develop subnet"
+  prohibit_public_ip_on_vnic = false
+  dns_label                  = "dev"
+}
+
+data "oci_core_images" "latest_images" {
+  compartment_id   = var.compartment_id
+  operating_system = "Canonical Ubuntu"
+
+  filter {
+    name   = "display_name"
+    values = ["^Canonical-Ubuntu-24.04-Minimal-.*$"]
+    regex  = true
   }
 
-  resource "oci_core_vcn" "internal" {
-    dns_label      = "internal"
-    cidr_block     = "172.16.0.0/20"
-    compartment_id = var.compartment_id
-    display_name   = "My internal VCN"
+  shape      = "VM.Standard.E4.Flex"
+  sort_by    = "TIMECREATED"
+  sort_order = "DESC"
+}
+
+resource "oci_core_instance" "terraform-testing" {
+  availability_domain = "Toma"
+  compartment_id      = var.compartment_id
+
+  shape = "VM.Standard.E4.Flex"
+  shape_config {
+    ocpus         = 1
+    memory_in_gbs = 6
   }
 
-  resource "oci_core_subnet" "dev" {
-    vcn_id                     = oci_core_vcn.internal.id
-    cidr_block                 = "172.16.0.0/24"
-    compartment_id              = var.compartment_id
-    display_name               = "Develop subnet"
-    prohibit_public_ip_on_vnic = true
-    dns_label                  = "dev"
+  source_details {
+    source_type = "image"
+    source_id   = data.oci_core_images.latest_images.images[0].id
   }
+
+  create_vnic_details {
+    subnet_id = oci_core_subnet.dev.id
+
+    assign_public_ip = true
+  }
+
+  metadata = {
+    ssh_authorized_keys = file("~/.ssh/id_rsa.pub")
+  }
+
+  display_name = "terraform server"
+}
